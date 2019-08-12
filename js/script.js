d134b4af12e00399096c35f45c6468fcb2eba94c
BeCrutch <roman.naumenko.94@gmail.com>
2019-08-13 00:30:31 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ console.log('javascript work!');
 			particleColor       : 'rgba(255, 40, 40, 1)',
 			particleRadius      : 3,
 			particleCount       : 60,
-			particleMaxVelocity : 0.5
+			particleMaxVelocity : 0.5,
+			lineLength   		: 150,
+			particleLife   		: 6
 		};
 
 	//создание в html canvas
@@ -27,7 +29,17 @@ console.log('javascript work!');
 		constructor(){
 			this.x = Math.random()*w;
 			this.y = Math.random()*h;
-			this.velocityX = Math.random()*(properties.particleMaxVelocity*2);// остановился!
+			this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+			this.velocityY = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+			this.life = Math.random()*properties.particleLife*60;
+		}
+
+		//метод обновляющий позиции
+		position() {
+			this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX*=-1 : this.velocityX;
+			this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY*=-1 : this.velocityY;
+			this.x += this.velocityX;
+			this.y += this.velocityY; 
 		}
 
 		//отрисовка частицы на canvas
@@ -38,23 +50,63 @@ console.log('javascript work!');
 			ctx.fillStyle = properties.particleColor;
 			ctx.fill();
 		}
-	}
+
+		//метод отнимающий жизнь
+		reCalculateLife() {
+			if(this.life < 1) {
+				this.x = Math.random()*w;
+				this.y = Math.random()*h;
+				this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+				this.velocityY = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+				this.life = Math.random()*properties.particleLife*60;
+			}
+			this.life--;
+		}
+
+
+	};
 
 	function reDrawBackground() {
 		ctx.fillStyle = properties.bgColor; //цвет заливки canvas
 		ctx.fillRect(0, 0, w, h); //прямоугольник
 	};
 
+	function drawLines() {
+		var x1, y1, x2, y2, length,	opacity;
+		for(var i in particles) {
+			for(var j in particles) {
+				x1 = particles[i].x;
+				y1 = particles[i].y;
+				x2 = particles[j].x;
+				y2 = particles[j].y;
+				length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+				if(length < properties.lineLength) {
+					opacity = 1 - length / properties.lineLength;
+					ctx.lineWidth   = '0.5';
+					ctx.strokeStyle = 'rgba(255, 40, 40, '+opacity+')';
+					ctx.beginPath();
+					ctx.moveTo(x1, y1);
+					ctx.lineTo(x2, y2);
+					ctx.closePath();
+					ctx.stroke();
+				};
+			};
+		};
+	};
+
 
 	function reDrawParticles() {
 		for(let i in particles) {
+			//particles[i].reCalculateLife();
+			particles[i].position();
 			particles[i].reDraw();
 		}
-	}
+	};
 
 	function loop() {
 		reDrawBackground();
 		reDrawParticles();
+		drawLines();
 		requestAnimationFrame(loop);
 	};
 
